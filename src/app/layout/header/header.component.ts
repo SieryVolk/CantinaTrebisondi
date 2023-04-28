0f6ea8865f281a39348755988faa6aec32b2a9c2
SieryVolk <lucadeias90@yahoo.it>
2023-04-28 11:57:30 +0200

    menu vini adattato al mobile

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  constructor() {}
+
+  mobile: boolean = false;
+  menuViniToggle: boolean = false;
+
+  ngOnInit() {
+    this.verificaMobile()
+  }
+
+  verificaMobile() {
+    if (window.innerWidth < 1000) {
+      this.mobile = true
+      // console.log('Dimensioni schermo =', window.innerWidth, "px")
+    }
+  }
+
+  menuViniMobile() {
+    if (!this.menuViniToggle) {
+    document.getElementById('dropVini')!.style.display = 'block'
+    } else document.getElementById('dropVini')!.style.display = 'none'
+    this.menuViniToggle = !this.menuViniToggle
+  }
 }
